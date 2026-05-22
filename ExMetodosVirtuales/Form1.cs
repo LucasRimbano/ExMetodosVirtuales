93059dff21d33e1f491c6a2362b98f2aad55d637
Lucas Rimbano <Lucasrimbano@gmail.com>
2026-05-22 10:56:51 -0300

    Implementa consigna 3 y agrega capturas de colores aleatorios

--- ExMetodosVirtuales/Form1.cs
+++ ExMetodosVirtuales/Form1.cs
@@ -9,20 +9,31 @@ namespace Figuras
     public partial class Form1 : Form
     {
         Figura[] figuras;
+        Random random = new Random();
 
         public Form1()
         {
             InitializeComponent();
 
-            this.Text = "Colores distintos";
+            this.Text = "Colores aleatorios rgb";
 
             figuras = new Figura[3] 
             {
-                new Circulo(60,Color.Red),
-                new Rectangulo(30,50,Color.Blue),
-                new Cuadrado(45,Color.Green),
+                new Circulo(60,ColorAleatorio()),
+                new Rectangulo(30,50,ColorAleatorio()),
+                new Cuadrado(45,ColorAleatorio()),
             };
 
+        }
+
+        private Color ColorAleatorio()
+            {
+             return Color.FromArgb(
+                 random.Next(256),
+                 random.Next(256),
+                 random.Next(256)
+
+                 );
         }
 
         private void button1_Click(object sender, EventArgs e)
